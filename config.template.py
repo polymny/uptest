6bf1522ad61590622da99a10984316bf02978780
Thomas Forgione <thomas@forgione.fr>
2023-01-27 10:42:07 +0100

    Adds template of config

---
+++ config.template.py
@@ -0,0 +1,12 @@
+mailer = {
+    "username": "contact@example.com",
+    "password": "mypassword",
+    "host": "smtp.example.com",
+    "port": "465",
+    "dest": "contact@example.com",
+}
+
+urls = [
+    "example.com",
+    "example.com/toto",
+]
